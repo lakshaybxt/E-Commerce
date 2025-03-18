30d07be32dd765aa1eb631b177cb42c7161710c6
lakshaybxt <bishtlakshay14@gmail.com>
2025-03-18 08:22:15 +0530

    Create a seperate function for delivery date

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,8 +3,9 @@ import { products, getProduct } from '../../data/products.js';
 import formatCurrency from '.././utils/money.js';
 // import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOption.js';
+import { deliveryOptions, getDeliveryOption, calculateDeliveryDate } from '../../data/deliveryOption.js';
 import { renderPaymentSummary } from './paymentSummary.js';
+import { renderCheckoutHeader } from './checkoutHeader.js';
 
 // hello();
 
@@ -24,9 +25,7 @@ export function renderOrderSummary() {
         const deliveryOptionId = cartItem.deliveryOptionId;
         const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-        const today = dayjs();
-        const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
-        const dateString = deliveryDate.format('dddd, MMMM D');
+        const dateString = calculateDeliveryDate(deliveryOption);
 
         // console.log(matchingProduct);
         cartSummayHTML += `
@@ -77,9 +76,9 @@ export function renderOrderSummary() {
         let html = ``;
 
         deliveryOptions.forEach((deliveryOption) => {
-            const today = dayjs();
-            const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
-            const dateString = deliveryDate.format('dddd, MMMM D');
+            const dateString = calculateDeliveryDate(deliveryOption);
+
+            // console.log(matchingProduct);
 
             const priceString = (deliveryOption.priceCents === 0) ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)}`; 
             const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
@@ -118,19 +117,20 @@ export function renderOrderSummary() {
                 removeFromCart(productId);
                 
                 renderOrderSummary();
-                updateCartQuantity();
+                // updateCartQuantity();
                 renderPaymentSummary();
+                renderCheckoutHeader();
             });
         });
 
-    function updateCartQuantity() {
-        const cartQuantity = calculateCartQuantity();
+    // function updateCartQuantity() {
+    //     const cartQuantity = calculateCartQuantity();
         
-        document.querySelector('.js-return-to-home-link').innerHTML = `${cartQuantity} items`;
-    } 
+    //     document.querySelector('.js-return-to-home-link').innerHTML = `${cartQuantity} items`;
+    // } 
 
 
-    updateCartQuantity();
+    // updateCartQuantity();
 
     document.querySelectorAll('.js-update-quantity-link')
         .forEach((link) => {
