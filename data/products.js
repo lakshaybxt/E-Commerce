c670f49219147935aefe44476d0be110e959e8f0
lakshaybxt <bishtlakshay14@gmail.com>
2025-03-23 16:06:33 +0530

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -77,7 +77,7 @@ export let products = [];
 
 export function loadProductsFetch() {
   const promise = fetch(
-    'https://supersimplebackend.dev/products'
+    'https://supersimplebackend.dev/producsts'
   ).then((response) => {
     return response.json();
 
