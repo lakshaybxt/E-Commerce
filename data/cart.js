63eda915b075d1ac2ba44a46831af38161aefa02
lakshaybxt <bishtlakshay14@gmail.com>
2025-03-24 12:46:08 +0530

    Generate the Order page HTML

--- data/cart.js
+++ data/cart.js
@@ -92,3 +92,11 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
   matchingItem.deliveryOptionId = deliveryOptionId;
   saveToStorage();
 }
+
+export async function loadCartFetch() {
+  const response = await fetch('https://supersimplebackend.dev/cart');
+  const text = await response.text();
+  console.log(text);
+
+  return text;
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,15 +2,16 @@ import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { loadProductsFetch } from '../data/products.js';
-// import { loadCart } from '../data/cart-class.js';
+import { loadCartFetch } from '../data/cart.js';
 // import '../data/car.js';
 // import '../data/backend-practice.js';
 
 async function loadPage() {
     try {
-        // throw 'error1'
-
-        await loadProductsFetch ();
+        await Promise.all([
+            loadProductsFetch(),
+            loadCartFetch()
+        ]);
     } catch (error) {
         console.log('Unexpected error. Please try again later.');
     }
@@ -23,12 +24,8 @@ loadPage();
 
 /*
 Promise.all([
-    loadProductsFetch (),
-    // new Promise((resolve) => {
-    //     loadCart(() => {
-    //         resolve();
-    //     })
-    // })
+    loadProductsFetch(),
+    loadCartFetch()
 
 ]).then(() => {
     renderCheckoutHeader();
