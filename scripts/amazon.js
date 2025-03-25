456ed2c8e0d850ca52b764ffc51f2a189b1e1887
lakshaybxt <bishtlakshay14@gmail.com>
2025-03-25 17:59:41 +0530

    Make the seach input in home space to able search

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -8,7 +8,25 @@ loadProductsFetch().then(() => {
 function renderProductsGrid() {
   let productsHTML = ``;
 
-  products.forEach((product) => {
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search');
+
+  let filteredProducts = products;
+
+  if(search) {
+    filteredProducts = products.filter((product) => {
+      let matchingKeyword = false;
+
+      product.keywords.forEach((keyword) => {
+        if(keyword.toLowerCase().includes(search.toLowerCase())) {
+          matchingKeyword = true;
+        }
+      });
+      return matchingKeyword || product.name.toLowerCase().includes(search.toLowerCase());
+    });
+  }
+
+  filteredProducts.forEach((product) => {
       productsHTML += `
           <div class="product-container">
             <div class="product-image-container">
@@ -97,4 +115,9 @@ function renderProductsGrid() {
           addedMessageTImeoutId = timeoutId;
       });
   });
+  document.querySelector('.js-search-button')
+    .addEventListener('click', () => {
+      const search = document.querySelector('.js-search-bar').value;
+      window.location.href = `amazon.html?search=${search}`;
+    })
 }
